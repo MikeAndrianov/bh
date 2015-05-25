4630002545b16ab31316420fb525791833fee205
Marina Harlach <marina.harlach@gmail.com>
2015-05-25 14:18:48 +0300

    authorization with VK and OAuth

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 gem 'devise'
+gem 'omniauth-vkontakte'
 
 gem 'ember-rails'
 gem 'ember-source'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:vkontakte]
 
   %w(manager hunter).each do |role|
     define_method("#{role}?") do
