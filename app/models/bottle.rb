fafd648e156089ec4d3e3624e9df58ffd39869f9
Mike Andrianov <mikeandrianov@gmail.com>
2015-05-25 22:09:03 +0300

    added bottle entity

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,5 +1,18 @@
 class Bottle < ActiveRecord::Base
   STATUSES = %w(available found returned)
 
+  # TODO: add coordinates, where bottle was fould!
+
   validates :status, presence: true
+  belongs_to :user
+
+  # TODO: update_attribute doesn't call any callbacks. 
+  # This method possibly should be refactoried in the future
+  def change_bottle_status
+    if user.hunter?
+      update_attribute(:status, 'found')
+    elsif user.manager?
+      update_attribute(:status, 'returned')
+    end
+  end
 end
